4b6c5962ca76e2466669c3e6acff16f415028a45
Vladimir <kyrytin@gmail.com>
2022-05-09 17:30:33 +0200

    fix: new element undefined tag

--- src/scripts/modules/Element.js
+++ src/scripts/modules/Element.js
@@ -1,6 +1,6 @@
 export default class Element {
-  constructor(parent, options = { tag: 'div', class: null }) {
-    const element = document.createElement(options.tag);
+  constructor(parent, options) {
+    const element = document.createElement(options.tag || 'div');
     if (options.class) element.classList.add(options.class);
     parent.append(element);
     this.node = element;
